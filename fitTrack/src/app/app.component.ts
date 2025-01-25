746ae3ff81a9e5201f4ea2bc3d80cd7fe03d1e0d
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-25 11:14:00 +0100

    Se cargar en app.component los imports de cada APP.

--- fitTrack/src/app/app.component.ts
+++ fitTrack/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormExerciseComponent } from './components/form-exercise/form-exercise.component';
+import { TotalComponent } from './components/total/total.component';
+import { ExerciseListComponent } from './components/exercise-list/exercise-list.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormExerciseComponent, TotalComponent, ExerciseListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
