1768cad3d0eec6829542102cd7ea282961227ac7
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-25 23:56:28 +0100

    se crea proyectó DataSphere todos los components, service e interface

---
+++ DataSphere/src/app/componentes/total-user/total-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-total-user',
+  imports: [],
+  templateUrl: './total-user.component.html',
+  styleUrl: './total-user.component.css'
+})
+export class TotalUserComponent {
+
+}
---
+++ DataSphere/src/app/interfaces/iuser.interface.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    id?: number
+    name: string
+    lastname: string
+    correo: string
+    fecha: string
+    genero: string
+    direccion: string
+    redsocial: string
+}
