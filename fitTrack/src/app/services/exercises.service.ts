3f1a6c147e93bbd15155e2834c230d706dcfbc28
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-25 11:45:11 +0100

    función inserta completa, que encapsular para transformar en 2 funciones.

--- fitTrack/src/app/services/exercises.service.ts
+++ fitTrack/src/app/services/exercises.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { IExercise } from '../interfaces/iexercise.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExercisesService {
 
-  constructor() { }
+  private arrExercises: IExercise[] = [];
+  private id: number = 1;
+
+  insertExercise(newexercise: IExercise): any {
+    let busqueda = this.arrExercises.find(exercise => exercise.title.toLocaleLowerCase() === newexercise.title.toLocaleLowerCase());
+    if (busqueda === undefined) {
+      newexercise.id = this.id;
+      this.arrExercises.push(newexercise);
+      this.id++;
+      return { status: 200, message: 'Ejercicio añadido correctamente' };
+    } else {
+      busqueda.quantity += newexercise.quantity;
+      return { status: 200, message: 'Ejercicio actualizado correctamente' };
+    }
+
+
+  }
 }
+
+
+
+
