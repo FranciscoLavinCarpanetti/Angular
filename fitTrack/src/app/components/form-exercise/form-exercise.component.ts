503b83c26deb70485779dc124e134e67da4632a6
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-25 20:20:52 +0100

    commit función getDataExercise

--- fitTrack/src/app/components/form-exercise/form-exercise.component.ts
+++ fitTrack/src/app/components/form-exercise/form-exercise.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { ExercisesService } from './../../services/exercises.service';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-exercise',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-exercise.component.html',
   styleUrl: './form-exercise.component.css'
 })
 export class FormExerciseComponent {
+  exerciseForm: FormGroup;
+
+  ExercisesService = inject(ExercisesService);
+
+  constructor() {
+    this.exerciseForm = new FormGroup({
+      title: new FormControl('', []),
+      quantity: new FormControl('', []),
+      weight: new FormControl('', [])
+    }, []);
+  }
+
+
+  getDataExercise() {
+    const respuesta = this.ExercisesService.insertExercise(this.exerciseForm.value);
+    alert(respuesta.message);
+    this.exerciseForm.reset();
+
+  }
 
 }
