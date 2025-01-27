2157ee71c689123de76ad1d0337ec62209506c98
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-01-27 11:13:19 +0100

    rutas, enlaces nav home-contacto-galeria

--- rutas/src/app/app.routes.ts
+++ rutas/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './peges/home/home.component';
+import { ContactoComponent } from './peges/contacto/contacto.component';
+import { GaleriaComponent } from './peges/galeria/galeria.component';
+import { C404Component } from './peges/c404/c404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: '**', redirectTo: 'contacto' }
+
+];
