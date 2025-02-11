f32fd79f3f981cefb85104e04404639c0312ce8b
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-11 17:10:18 +0100

    creacion de component page/users/register

---
+++ Store/src/app/pages/users/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../../../services/users.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  usersService = inject(UsersService)
+  router = inject(Router)
+
+
+
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  async onSubmit() {
+
+    try {
+      const response = await this.usersService.login(this.loginForm.value);
+      Swal.fire('Login', 'Usuario logueado correctamente', 'success');
+      localStorage.setItem(environment.tokenName, response.token);
+      this.router.navigateByUrl('/product');
+
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Login', 'ha ocurrido un error en el login vuelve a intentarlo.', 'error');
+    }
+
+  }
+
+
+
+}
