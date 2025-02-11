8b3b902dab7c4cca0a97ac0b00923341a590d225
Francisco Javier Lavin <francisco_javier_lavin@hotmail.com>
2025-02-11 15:07:42 +0100

    Configuración de rutas para distintas paginas, 'Login', 'Register', 'Products', 'Products/new'

--- Store/src/app/app.routes.ts
+++ Store/src/app/app.routes.ts
@@ -1,10 +1,24 @@
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/users/register/register.component';
+import { LoginComponent } from './pages/users/login/login.component';
+import { NewProductComponent } from './pages/products/new-product/new-product.component';
+import { authGuard } from './guards/auth.guard';
+import { adminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/register' },
   {
     path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'products', component: ProductsListComponent
+  },
+  {
+    path: 'products/new', component: NewProductComponent, canActivate: [authGuard, adminGuard]
   },
   { path: '**', redirectTo: '/register' }
 
